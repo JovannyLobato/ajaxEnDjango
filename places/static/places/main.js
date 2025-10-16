ead2253917786b3258637a2a7a7ff2630ea11c35
JovannyLobato <jovannylobato2004@gmail.com>
2025-10-16 00:42:15 -0600

    agrega vista principal y el scrip js con ajax para cargar los municipios

---
+++ places/static/places/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $('#estado').change(function () {
+        var estadoId = $(this).val();
+        $('#municipio').empty();
+
+        if (estadoId) {
+            $.ajax({
+                url: '/cargar-municipios/',
+                data: { 'estado_id': estadoId },
+                success: function (data) {
+                    $('#municipio').append('<option value="">--- Selecciona ---</option>');
+                    data.forEach(function (municipio) {
+                        $('#municipio').append('<option value="' + municipio.id + '">' + municipio.nombre + '</option>');
+                    });
+                }
+            });
+        } else {
+            $('#municipio').append('<option value="">--- Selecciona un estado primero ---</option>');
+        }
+    });
+});
+
